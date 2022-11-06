82104ea8bceb9cb4549ffeef4942ec0818df3f53
Youssef Ewady <ewady@Youssefs-MacBook-Pro-2.local>
2022-11-06 19:02:47 +0200

    Add config test message

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello AWS => config service enabled!';
+    return `Hello AWS => config service enabled... => ${this.configService.get(
+      'ENV_TEST_VERIFICATION_MESSAGE',
+    )}`;
   }
 }
