0e0d9ad4df64f6e64dc81f42b15a5ace7a53cda9
Youssef Ewady <ewady@Youssefs-MacBook-Pro-2.local>
2022-11-06 18:11:53 +0200

    Modify main.ts function

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+
   const port = process.env.PORT || 3000;
+
   await app.listen(port);
-}
+};
+
 bootstrap();
